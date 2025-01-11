7cf901f912579c253947995e38847f7c59615e4e
Tomasz Ż. <tzuber95@gmail.com>
2025-01-11 12:41:17 +0100

    Create about us section on homepage

---
+++ src/app/about-us/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+    title: 'PZB Law Firm | About us',
+    description:
+        'Providing exceptional legal services across various practice areas, including corporate law, family law, real estate, and litigation. Our team of experienced attorneys is dedicated to delivering tailored solutions with professionalism and integrity.',
+}
+
+const AboutusPage = () => {
+    return <main>a</main>
+}
+
+export default AboutusPage
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+    title: 'PZB Law Firm | Contact',
+    description:
+        'Providing exceptional legal services across various practice areas, including corporate law, family law, real estate, and litigation. Our team of experienced attorneys is dedicated to delivering tailored solutions with professionalism and integrity.',
+}
+
+const ContactPage = () => {
+    return <main>a</main>
+}
+
+export default ContactPage
